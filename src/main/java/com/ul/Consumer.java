cad7ec2bb98e41bf6f9f7a890f89a6de97120154
L. Rhode <lrh@certi.org.br>
2021-12-09 10:59:07 -0300

    Adds buffer and prioritizing mechanism

--- src/main/java/com/ul/Consumer.java
+++ src/main/java/com/ul/Consumer.java
@@ -3,15 +3,29 @@
  */
 package com.ul;
 
-import java.util.concurrent.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.concurrent.BlockingQueue;
 
 public class Consumer {
+    private final int BUFFER_SIZE = 10;
 
     private BlockingQueue<Message> queue;
     private Thread consumerThread = null;
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ");
+
+    private ArrayList<Message> buffer = new ArrayList<>();
 
     public Consumer(BlockingQueue<Message> queue) {
         this.queue = queue;
+    }
+
+    private void formatAndLog(Message message) {
+        String date = this.simpleDateFormat.format(new Date(message.getTimestamp()));
+        String output = String.format("%s [%s]: %s", date, message.getPriority(), message.getText());
+        System.out.println(output);
     }
 
     public void startConsuming() {
@@ -21,7 +35,7 @@ public class Consumer {
                 while (true) {
                     try {
                         Message message = queue.take();
-                        System.out.println(message);
+                        Consumer.this.addToBuffer(message);
                     } catch (InterruptedException e) {
                         // executing thread has been interrupted, exit loop
                         break;
@@ -34,5 +48,20 @@ public class Consumer {
 
     public void stopConsuming() {
         consumerThread.interrupt();
+    }
+
+    private void flush() {
+        Collections.sort(buffer);
+        for (Message message : buffer) {
+            formatAndLog(message);
+        }
+        buffer.clear();
+    }
+
+    private void addToBuffer(Message message) {
+        buffer.add(message);
+        if (buffer.size() >= BUFFER_SIZE) {
+            flush();
+        }
     }
 }
